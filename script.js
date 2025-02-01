748041f20238c9afd3bf662458428b5d6a39ad96
vladyslav40 <86410387+vladyslav40@users.noreply.github.com>
2025-02-01 12:17:05 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// Анимация при прокрутке
 document.addEventListener("DOMContentLoaded", function () {
     const timelineItems = document.querySelectorAll(".timeline-item");
 
+    // Плавная анимация при прокрутке
     function revealOnScroll() {
         const windowHeight = window.innerHeight;
         timelineItems.forEach(item => {
